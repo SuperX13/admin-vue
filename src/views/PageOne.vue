aecb417a058de9bd9524eafba3e89bb3b1ee72af
yongxin <965224591@qq.com>
2021-05-02 00:42:52 +0800

    前后端分离前端VUE

--- src/views/PageOne.vue
+++ src/views/PageOne.vue
@@ -16,26 +16,31 @@
             }
         },
         mounted() {
-            let chartOne=this.$echarts.init(document.getElementById('chartOne'));
-            let option;
-            option = {
-                xAxis: {
-                    type: 'category',
-                    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-                },
-                yAxis: {
-                    type: 'value'
-                },
-                series: [{
-                    data: [120, 200, 150, 80, 70, 110, 130],
-                    type: 'bar',
-                    showBackground: true,
-                    backgroundStyle: {
-                        color: 'rgba(180, 180, 180, 0.2)'
-                    }
-                }]
-            };
-            chartOne.setOption(option);
+            let _this=this
+            axios.get("http://localhost:8181/supermarket/barVo").then(function (response) {
+                let chartOne=_this.$echarts.init(document.getElementById('chartOne'));
+                let option;
+                option = {
+                    xAxis: {
+                        type: 'category',
+                        data: response.data.names
+                    },
+                    yAxis: {
+                        type: 'value'
+                    },
+                    series: [{
+                        data: response.data.values,
+                        type: 'bar',
+                        showBackground: true,
+                        backgroundStyle: {
+                            color: 'rgba(180, 180, 180, 0.2)'
+                        }
+                    }]
+                };
+                chartOne.setOption(option);
+            })
+
+
         }
     }
 </script>
